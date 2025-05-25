3cd0e0bc97c8e489406b7d9222590e2036fdc63c
RakibHossen678 <rh491464@gmail.com>
2025-05-25 20:40:18 +0600

    Solved: problem 8,9

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE wildLife
-
 CREATE TABLE rangers (
     ranger_id SERIAL,
     ranger_name VARCHAR(255),
@@ -109,34 +107,77 @@ VALUES (
         NULL
     );
 
-SELECT * FROM rangers;
-
-SELECT * FROM sightings;
-
-SELECT * FROM species;
-
+-- Problem - 1
 INSERT INTO
     rangers (ranger_name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
 
+-- Problem - 2
 SELECT count(DISTINCT species_id) FROM sightings;
 
+-- Problem - 3
 SELECT * FROM sightings WHERE location ILIKE '%pass%';
 
+-- Problem - 4
 SELECT ranger_name, count(sighting_id)
 FROM sightings
     JOIN rangers ON sightings.ranger_id = rangers.ranger_id
 GROUP BY
     ranger_name;
 
+-- Problem - 5
 SELECT common_name
 FROM species
     LEFT JOIN sightings ON species.species_id = sightings.species_id
 WHERE
     sightings.species_id IS NULL;
 
+-- Problem - 6
+SELECT
+    common_name,
+    sighting_time,
+    ranger_name
+FROM
+    sightings
+    JOIN species ON sightings.species_id = species.species_id
+    JOIN rangers ON sightings.ranger_id = rangers.ranger_id
+ORDER BY sighting_time DESC
+LIMIT 2;
 
-SELECT common_name , sighting_time,ranger_name FROM sightings JOIN species ON sightings.species_id = species.species_id JOIN rangers ON sightings.ranger_id = rangers.ranger_id ORDER BY sighting_time DESC LIMIT 2;
+-- Problem - 7
+UPDATE species
+SET
+    conservation_status = 'Historic'
+WHERE
+    extract(
+        year
+        FROM discovery_date
+    ) < 1800;
 
+-- Problem - 8
+SELECT
+    sighting_id,
+    sighting_time,
+    case
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) <= 12 THEN 'Morning'
+        WHEN EXTRACT(
+            HOUR
+            FROM sighting_time
+        ) <= 15 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings
 
-UPDATE 
+-- Problem - 9
+DELETE From rangers
+WHERE
+    ranger_id IN (
+        SELECT rangers.ranger_id
+        FROM rangers
+            LEFT JOIN sightings ON rangers.ranger_id = sightings.ranger_id
+        WHERE
+            sightings.ranger_id IS NULL
+    )
